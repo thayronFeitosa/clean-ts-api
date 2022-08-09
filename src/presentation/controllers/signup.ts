d4a64492b632b3b683314d64a9e081162278ec7e
thayron.matos <thayronf3@gmail.com>
2022-08-09 06:59:29 -0300

    feat: ensure SignUpController return 400 if passwordConfirmation faild

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -20,6 +20,10 @@ export class SignUpController implements IController {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.password_confirmation) {
+        return badRequest(new InvalidParamError('password_confirmation'))
+      }
+
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
 
       if (!isValid) return badRequest(new InvalidParamError('email'))

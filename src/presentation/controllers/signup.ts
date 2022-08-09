4f7a80f4e402c998368e8b41166f6df0744e565a
thayron.matos <thayronf3@gmail.com>
2022-08-09 01:35:10 -0300

    feat: ensure SignUpController return 400 if an invalid email provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,32 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { IController } from '../protocols/controller'
+import { IEmailValidator } from '../protocols/email-validator'
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 
 export class SignUpController implements IController {
-  handle (httpRequest: IHttpRequest): IHttpResponse {
-    const requiresFildes = ['name', 'email', 'password', 'password_confirmation']
+  private readonly emailValidator: IEmailValidator
 
-    for (const field of requiresFildes) {
+  constructor (emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle (httpRequest: IHttpRequest): IHttpResponse {
+    const requiresFields = ['name', 'email', 'password', 'password_confirmation']
+
+    for (const field of requiresFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    console.log(isValid)
+
+    if (!isValid) return badRequest(new InvalidParamError('email'))
+
     return {
       statusCode: 400,
       body: true

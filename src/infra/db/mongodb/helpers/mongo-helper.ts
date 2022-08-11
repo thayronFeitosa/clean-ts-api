eefa1bbca79e1395eecebe852077162aea34538b
thayron.matos <thayronf3@gmail.com>
2022-08-11 06:39:53 -0300

    feat: create MongoHelper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,28 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    const URI = process.env.MONGO_URL ?? uri
+
+    this.client = await MongoClient.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    await this.client.db()
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  }
+}

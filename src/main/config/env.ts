0d63181183831672471af1233ad97048e8e11561
thayron matos <thayron.matos@soluti.com.br>
2022-09-07 15:09:28 -0300

    fix: add mongo in docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export default {
-  mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://170.20.0.11:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://172.20.0.2:27017/clean-node-api',
   port: process.env.PORT ? process.env.PORT : 5050
 }

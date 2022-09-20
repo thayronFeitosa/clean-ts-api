7dde6078d0df967247b853655e5b1a2e269e7eec
thayron matos <thayron.matos@soluti.com.br>
2022-09-20 05:06:43 -0300

    refactor: move files for main

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,5 +1,5 @@
-import { ILogErrorRepository } from '../data/protocols/log-error-respository'
-import { IController, IHttpRequest, IHttpResponse } from '../presentation/protocols'
+import { ILogErrorRepository } from '../../data/protocols/log-error-respository'
+import { IController, IHttpRequest, IHttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements IController {
   private readonly controller: IController
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,7 +4,7 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { IController } from '../../presentation/protocols'
-import { LogControllerDecorator } from '../../decorators/log'
+import { LogControllerDecorator } from '../decorators/log'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSignUpController = (): IController => {

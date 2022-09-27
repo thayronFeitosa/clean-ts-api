87ca38d68adfae1c64c4352158ecc6f9750bcb2a
thayron matos <thayron.matos@soluti.com.br>
2022-09-27 06:11:25 -0300

    feat: ensure LoginController return 400 if no password is provided

--- src/presentation/controllers/signup/login/login.ts
+++ src/presentation/controllers/signup/login/login.ts
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { MissingParamError } from '../../../errors'
 import { badRequest } from '../../../helpers/http-helper'
 import { IHttpRequest, IHttpResponse, IController } from '../../../protocols'
 
 export class LoginController implements IController {
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return await new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+    if (!httpRequest.body.email) {
+      return await new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+    }
+
+    if (!httpRequest.body.password) {
+      return await new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+    }
+    return {
+      body: 'ok',
+      statusCode: 200
+    }
   }
 }

cd24c3af21a1b4f356ff5f4ba79b9ef2ee0c169e
thayron matos <thayron.matos@soluti.com.br>
2022-09-27 06:22:18 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/signup/login/login.ts
+++ src/presentation/controllers/signup/login/login.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { MissingParamError } from '../../../errors'
 import { badRequest } from '../../../helpers/http-helper'
-import { IHttpRequest, IHttpResponse, IController } from '../../../protocols'
+import { IHttpRequest, IHttpResponse, IController, IEmailValidator } from '../../../protocols'
 
 export class LoginController implements IController {
+  private readonly emailValidator: IEmailValidator
+  constructor (emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     if (!httpRequest.body.email) {
       return await new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
@@ -12,6 +17,7 @@ export class LoginController implements IController {
     if (!httpRequest.body.password) {
       return await new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid(httpRequest.body.email)
     return {
       body: 'ok',
       statusCode: 200

1804e31600305cc25da8b1d651e0516e8bdb9e5e
thayron matos <thayronf3@gmail.com>
2022-11-13 19:15:21 -0300

    feat: ensure DbAuthentication calls ToeknGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,20 +1,24 @@
 import { IAuthentication, IAuthenticationModel } from '../../../domain/usecases/authentication'
 import { IHashCompare } from '../../protocols/criptography/hash-compare'
+import { ITokenGenerator } from '../../protocols/criptography/toeken-generator'
 import { ILoadAccountBYEnailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements IAuthentication {
   private readonly loadAccountBYEnailRepository: ILoadAccountBYEnailRepository
   private readonly hashCompare: IHashCompare
+  private readonly tokenGenerator: ITokenGenerator
 
-  constructor (loadAccountBYEnailRepository: ILoadAccountBYEnailRepository, hashCompare: IHashCompare) {
+  constructor (loadAccountBYEnailRepository: ILoadAccountBYEnailRepository, hashCompare: IHashCompare, tokenGenerator: ITokenGenerator) {
     this.loadAccountBYEnailRepository = loadAccountBYEnailRepository
     this.hashCompare = hashCompare
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: IAuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountBYEnailRepository.load(authentication.email)
     if (account != null) {
       await this.hashCompare.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
 
     return null

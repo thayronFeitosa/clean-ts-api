b0b6a267ce906bbdd97d44756181ba15935988fe
thayron matos <thayronf3@gmail.com>
2022-11-13 08:16:23 -0300

    feat: ensure DbAuthentication calls LoadAccountBYEnailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import { IAuthentication, IAuthenticationModel } from '../../../domain/usecases/authentication'
+import { ILoadAccountBYEnailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements IAuthentication {
+  private readonly loadAccountBYEnailRepository: ILoadAccountBYEnailRepository
+
+  constructor (loadAccountBYEnailRepository: ILoadAccountBYEnailRepository) {
+    this.loadAccountBYEnailRepository = loadAccountBYEnailRepository
+  }
+
+  async auth (authentication: IAuthenticationModel): Promise<string | undefined> {
+    await this.loadAccountBYEnailRepository.load(authentication.email)
+
+    return undefined
+  }
+}

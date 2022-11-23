eae08b3788bc0b21f6e8766b97a765ed6b99180e
thayron matos <thayronf3@gmail.com>
2022-11-23 06:58:51 -0300

    test: login route return 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,16 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on signup ', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rodrigo@email.com',
+          password: '123'
+
+        })
+        .expect(401)
+    })
   })
 })

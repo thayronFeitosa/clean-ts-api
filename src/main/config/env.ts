5a7617397133ddad064d8ee4cf527f4cf361fcb0
thayron matos <thayronf3@gmail.com>
2022-11-23 06:49:56 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export default {
   mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://127.0.0.1:27017/clean-node-api',
-  port: process.env.PORT ? process.env.PORT : 5050
+  port: process.env.PORT ? process.env.PORT : 5050,
+  jwtSecret: process.env.JWT_SECRET ? process.env.JWT_SECRET : 'asdf65#@$5kj&'
 }
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-router-adapter'
 
 export default (router: Router): void => {

508f1f7d38f926039488964166168ada6101ad6a
thayron matos <thayronf3@gmail.com>
2023-02-21 06:49:32 -0300

    fix: resolved bug to map files in routes config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,12 +2,20 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
 
+// export default (app: Express): void => {
+//   const router = Router()
+//   app.use('/api', router)
+//   readdirSync(`${__dirname}/../routes`).map(async file => {
+//     if (!file.includes('.test.')) {
+//       (await import(`../routes/${file}`)).default(router)
+//     }
+//   })
+// }
+
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
-      (await import(`../routes/${file}`)).default(router)
-    }
+  readdirSync(`${String(__dirname)}/../routes`).map(async (file) => {
+    if (!file.endsWith('.map') && !file.includes('.test.')) { (await import(`../routes/${file}`)).default(router) }
   })
 }

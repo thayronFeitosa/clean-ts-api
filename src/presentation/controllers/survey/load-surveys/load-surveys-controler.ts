2aee499d63739b053c8149af809ffb93d18799a8
thayron matos <thayronf3@gmail.com>
2023-09-19 05:41:43 -0300

    test: ensure LoadSurveysController returns 500 if LoadSurveusUseCase throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controler.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controler.ts
@@ -1,10 +1,14 @@
-import { ok } from '../../../helpers/http-helper'
+import { ok, serverError } from '../../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse, ILoadSurveys } from './load-surveys-protocols'
 
 export class LoadSurveysController implements IController {
   constructor (private readonly loadSurveys: ILoadSurveys) {}
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }

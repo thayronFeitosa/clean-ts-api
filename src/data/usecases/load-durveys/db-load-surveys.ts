23c4e16ca832506c1afeceb5797e0be064b4cc35
thayron matos <thayronf3@gmail.com>
2023-09-22 05:57:47 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-durveys/db-load-surveys.ts
@@ -0,0 +1,11 @@
+import { ISurveyModel } from '../../../domain/models/survey'
+import { ILoadSurveys } from '../../../domain/usecases/load-surveys'
+import { ILoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
+
+export class DbLoadSurvey implements ILoadSurveys {
+  constructor (private readonly loadSurveyRepository: ILoadSurveysRepository) {}
+  async load (): Promise<ISurveyModel[]> {
+    await this.loadSurveyRepository.loadAll()
+    return []
+  }
+}

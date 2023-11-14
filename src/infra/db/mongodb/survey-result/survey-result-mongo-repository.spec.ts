3c991238f0bcc23de21b6f3f31cdc4edbc395f14
thayron matos <thayronf3@gmail.com>
2023-11-14 04:15:26 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -16,15 +16,17 @@ const makeSurvey = async (): Promise<SurveyModel> => {
   const res = await surveyCollection.insertOne({
     question: 'any_question',
     answers: [{
-      answer: 'any_answer',
-      image: 'any_image'
+      image: 'any_image',
+      answer: 'any_answer'
+    }, {
+      answer: 'other_answer'
     }],
     date: new Date()
   })
   return res.ops[0]
 }
 
-const makeAccont = async (): Promise<AccountModel> => {
+const makeAccount = async (): Promise<AccountModel> => {
   const res = await accountCollection.insertOne({
     name: 'any_name',
     email: 'any_email@mail.com',
@@ -53,9 +55,8 @@ describe('save()', () => {
   describe('add()', () => {
     test('Should add a survey result if its new', async () => {
       const survey = await makeSurvey()
-      console.log(survey)
 
-      const account = await makeAccont()
+      const account = await makeAccount()
       const sut = makeSut()
       const surveyResult = await sut.save({
         surveyId: survey.id,
@@ -66,6 +67,28 @@ describe('save()', () => {
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+
+    test('Should update survey result if its not new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const rest = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(rest.ops[0]._id)
+      expect(surveyResult.answer).toBe(survey.answers[1].answer)
     })
   })
 })

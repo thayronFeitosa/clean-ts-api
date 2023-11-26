867272b0e0fb57cd379cd639fb7d731e0888e8b6
thayron matos <thayronf3@gmail.com>
2023-11-26 03:10:47 -0300

    feat: ensure SaveSruveyResultController calls SaveServeyResult with correct values

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -1,6 +1,6 @@
-import { ISaveSurveyResultRepository, SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel } from './db-save-survey-result-protocols'
+import { ISaveSurveyResultRepository, ISaveSurveyResult, SaveSurveyResultModel, SurveyResultModel } from './db-save-survey-result-protocols'
 
-export class DbSaveSurveyResult implements SaveSurveyResult {
+export class DbSaveSurveyResult implements ISaveSurveyResult {
   constructor (private readonly saveSurveyResultRepositoryStub: ISaveSurveyResultRepository) {}
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyResult = await this.saveSurveyResultRepositoryStub.save(data)
--- src/presentation/controllers/survey/survey-result/save-servey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/survey-result/save-servey-result/save-survey-result-controller.ts
@@ -1,14 +1,18 @@
 import { forbidden, serverError } from '@/presentation/helpers/http-helper'
-import { HttpRequest, HttpResponse, IController, ILoadSurveysById } from './save-survey-result-controller-protocols'
+import { HttpRequest, HttpResponse, IController, ILoadSurveysById, ISaveSurveyResult } from './save-survey-result-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements IController {
-  constructor (private readonly loadSurveysById: ILoadSurveysById) {}
+  constructor (
+    private readonly loadSurveysById: ILoadSurveysById,
+    private readonly saveSurveyResult: ISaveSurveyResult
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveysById.loadById(surveyId)
 
       if (survey) {
@@ -19,6 +23,12 @@ export class SaveSurveyResultController implements IController {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.saveSurveyResult.save({
+        accountId,
+        surveyId,
+        answer,
+        date: new Date()
+      })
       return null
     } catch (err) {
       return serverError(err)
